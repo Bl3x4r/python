45af74494a481f910f685639bbaa76c0b7751d9d
Bl3x4r <llanosb65@gmail.com>
2023-11-15 14:48:35 -0300

    dojos_y_ninjas

--- flask_mysql/crud/dojos_y_ninjas/server.py
+++ flask_mysql/crud/dojos_y_ninjas/server.py
@@ -1,6 +1,9 @@
 from flask_app.controllers import controlador_dojos, controlador_ninja
-from flask import render_template
+from flask import redirect
 from flask_app import app
 
+@app.route("/")
+def main():
+    return redirect("/dojos")
 if __name__ == "__main__":
     app.run(debug = True)
